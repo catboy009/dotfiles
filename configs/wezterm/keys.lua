77c302c3988c5b28c054aa061454ae09c8082cf1
catboy009 <catboy009@icloud.com>
2025-02-02 12:10:55 +0500

    aerospace

--- configs/wezterm/keys.lua
+++ configs/wezterm/keys.lua
@@ -2,63 +2,19 @@ local wezterm = require("wezterm")
 
 local keys = {}
 
-local function is_vi_process(pane)
-	return pane:get_foreground_process_name():find("n?vim") ~= nil
-end
-
-local direction_keys = {
-	Left = "h",
-	Down = "j",
-	Up = "k",
-	Right = "l",
-	h = "Left",
-	j = "Down",
-	k = "Up",
-	l = "Right",
-}
-
-local function split_nav(resize_or_move, key)
-	return {
-		key = key,
-		mods = resize_or_move == "resize" and "SHIFT|ALT" or "ALT",
-		action = wezterm.action_callback(function(win, pane)
-			if is_vi_process(pane) then
-				win:perform_action({
-					SendKey = { key = key, mods = resize_or_move == "resize" and "SHIFT|ALT" or "ALT" },
-				}, pane)
-			else
-				if resize_or_move == "resize" then
-					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-				else
-					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-				end
-			end
-		end),
-	}
-end
-
 function keys.setup(config)
-	config.disable_default_key_bindings = true
+	config.disable_default_key_bindings = false
 	config.keys = {
-		split_nav("move", "h"),
-		split_nav("move", "j"),
-		split_nav("move", "k"),
-		split_nav("move", "l"),
-
-		split_nav("resize", "h"),
-		split_nav("resize", "j"),
-		split_nav("resize", "k"),
-		split_nav("resize", "l"),
 
 		{
-			mods = "ALT",
+			mods = "CMD",
 			key = [[\]],
 			action = wezterm.action({
 				SplitHorizontal = { domain = "CurrentPaneDomain" },
 			}),
 		},
 		{
-			mods = "ALT|SHIFT",
+			mods = "CMD|SHIFT",
 			key = [[|]],
 			action = wezterm.action.SplitPane({
 				top_level = true,
@@ -67,14 +23,14 @@ function keys.setup(config)
 			}),
 		},
 		{
-			mods = "ALT",
+			mods = "CMD",
 			key = [[-]],
 			action = wezterm.action({
 				SplitVertical = { domain = "CurrentPaneDomain" },
 			}),
 		},
 		{
-			mods = "ALT|SHIFT",
+			mods = "CMD|SHIFT",
 			key = [[_]],
 			action = wezterm.action.SplitPane({
 				top_level = true,
@@ -82,30 +38,64 @@ function keys.setup(config)
 				size = { Percent = 50 },
 			}),
 		},
-		{ key = "n", mods = "ALT", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "Q", mods = "ALT", action = wezterm.action({ CloseCurrentTab = { confirm = false } }) },
-		{ key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-		{ key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
+		{
+			key = "h",
+			mods = "CMD",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "l",
+			mods = "CMD",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "k",
+			mods = "CMD",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "j",
+			mods = "CMD",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
 
-		{ key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-		{ key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-		{ key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
-		{ key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
+		{
+			key = "h",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+		},
+		{
+			key = "l",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+		},
+		{
+			key = "k",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+		},
+		{
+			key = "j",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+		},
+		{ key = "z", mods = "CMD", action = wezterm.action.TogglePaneZoomState },
+		{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
 
-		{ key = "y", mods = "ALT", action = wezterm.action.ActivateCopyMode },
-		{ key = "q", mods = "CMD", action = wezterm.action.QuitApplication },
-		{ key = "c", mods = "CMD", action = wezterm.action({ CopyTo = "Clipboard" }) },
-		{ key = "v", mods = "CMD", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		{ key = "[", mods = "CMD", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		{ key = "]", mods = "CMD", action = wezterm.action({ ActivateTabRelative = 1 }) },
 
-		{ key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
-		{ key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
-		{ key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
-		{ key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
-		{ key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
-		{ key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
-		{ key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
-		{ key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
-		{ key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
+		{ key = "y", mods = "CMD", action = wezterm.action.ActivateCopyMode },
+
+		{ key = "1", mods = "CMD", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "CMD", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "CMD", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "CMD", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "CMD", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "CMD", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "CMD", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "CMD", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "CMD", action = wezterm.action({ ActivateTab = 8 }) },
 	}
 end
 
--- home/default.nix
+++ home/default.nix
@@ -5,6 +5,7 @@
     wezterm
     iina
     keka
+    aerospace
     ripgrep
     pfetch-rs
     thokr
@@ -50,4 +51,67 @@
       "/Users/catboy/.dotfiles/configs/luakit";
     recursive = true;
   };
+  home.file.".aerospace.toml".text = ''
+    start-at-login = true
+    after-login-command = []
+    after-startup-command = []
+    enable-normalization-flatten-containers = true
+    enable-normalization-opposite-orientation-for-nested-containers = true
+    accordion-padding = 30
+    default-root-container-layout = 'tiles'
+    default-root-container-orientation = 'auto'
+    on-focused-monitor-changed = ['move-mouse monitor-lazy-center']
+    automatically-unhide-macos-hidden-apps = false
+
+    [key-mapping]
+    preset = 'qwerty'
+
+    [gaps]
+    inner.horizontal = 0
+    inner.vertical = 0
+    outer.left = 0
+    outer.bottom = 0
+    outer.top = 0
+    outer.right = 0
+
+    [mode.main.binding]
+
+    alt-h = 'focus left'
+    alt-j = 'focus down'
+    alt-k = 'focus up'
+    alt-l = 'focus right'
+
+    alt-shift-h = 'move left'
+    alt-shift-j = 'move down'
+    alt-shift-k = 'move up'
+    alt-shift-l = 'move right'
+
+    alt-shift-minus = 'resize smart -50'
+    alt-shift-equal = 'resize smart +50'
+
+    alt-1 = 'workspace 1'
+    alt-2 = 'workspace 2'
+    alt-3 = 'workspace 3'
+    alt-4 = 'workspace 4'
+    alt-5 = 'workspace 5'
+    alt-6 = 'workspace 6'
+    alt-7 = 'workspace 7'
+    alt-8 = 'workspace 8'
+    alt-9 = 'workspace 9'
+
+    alt-shift-1 = 'move-node-to-workspace 1'
+    alt-shift-2 = 'move-node-to-workspace 2'
+    alt-shift-3 = 'move-node-to-workspace 3'
+    alt-shift-4 = 'move-node-to-workspace 4'
+    alt-shift-5 = 'move-node-to-workspace 5'
+    alt-shift-6 = 'move-node-to-workspace 6'
+    alt-shift-7 = 'move-node-to-workspace 7'
+    alt-shift-8 = 'move-node-to-workspace 8'
+    alt-shift-9 = 'move-node-to-workspace 9'
+
+    alt-tab = 'workspace-back-and-forth'
+    alt-shift-tab = 'move-workspace-to-monitor --wrap-around next'
+
+    alt-shift-space = 'layout floating tiling'
+  '';
 }
